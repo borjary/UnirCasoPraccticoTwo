748228086b9bc0aa3198d6b78859f26fc58d56ea
Borja Romero <borja.romero@sngular.com>
2022-03-13 17:43:45 +0100

    Cambios

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,11 +1,11 @@
 variable "vm_size" {
-  type = string
+  type = list(string)
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  default =  ["Standard_D2s_v3","Standard_D1_v2","Standard_D1_v2"] 
 }
 
 variable "vms" {
   type = list(string)
   description = "Listado de maquinas virtuales a crear"
-  default = ["master","worker01","worker02","nfs"]
+  default = ["master","worker","nfs"]
 }

c15076f6b9743a6a952ff889c29bb11d56070ab3
Borja Romero <borja.romero@sngular.com>
2022-03-13 11:23:52 +0100

    refactor

---
+++ terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "vm_size" {
+  type = string
+  description = "Tamaño de la máquina virtual"
+  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+}
+
+variable "vms" {
+  type = list(string)
+  description = "Listado de maquinas virtuales a crear"
+  default = ["master","worker01","worker02","nfs"]
+}
